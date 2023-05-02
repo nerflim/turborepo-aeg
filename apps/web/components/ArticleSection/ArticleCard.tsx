be396d65d5a4da009786ee74bf9da3f90a0d777d
Milfren John dela Vega <nerflimjoh@gmail.com>
2023-05-02 22:37:48 +0800

    Article card hover transitions

--- apps/web/components/ArticleSection/ArticleCard.tsx
+++ apps/web/components/ArticleSection/ArticleCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import Image from 'next/image';
 
 interface ArticleCardProps {
@@ -9,32 +9,35 @@ interface ArticleCardProps {
 }
 
 const ArticleCard = ({ thumbnail, title, description }: ArticleCardProps) => {
-  const onReadMore = () => {
+  const onReadMore = (e: MouseEvent<HTMLAnchorElement>) => {
     console.log('Read more clicked!');
+    console.log(e.target);
   };
 
   return (
-    <div>
-      <Image
-        src={thumbnail}
-        alt={title}
-        width={384}
-        height={290}
-        className="object-contain border-b-4 border-primary"
-      />
+    <a
+      className="flex-1 group cursor-pointer hover:-translate-y-3 transition-transform duration-300"
+      onClick={onReadMore}
+    >
+      <div className="overflow-hidden border-b-4 border-primary group-hover:shadow-xl">
+        <Image
+          src={thumbnail}
+          alt={title}
+          width={384}
+          height={290}
+          className="w-full object-cover group-hover:scale-110 transition-transform duration-300"
+        />
+      </div>
       <div className="py-6">
         <div className="text-base mb-3">{title}</div>
         <div className="font-light leading-relaxed opacity-70">
           {description}
         </div>
       </div>
-      <a
-        className="cursor-pointer border-b border-primary py-3"
-        onClick={onReadMore}
-      >
+      <div className="cursor-pointer border-b border-primary py-3 inline-block group-hover:border-b-4 group-hover:font-bold transition-all">
         READ MORE
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
 
