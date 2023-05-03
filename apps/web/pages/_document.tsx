54e38c178874d0bb434f7aadc0152a6017166fb8
Milfren John dela Vega <nerflimjoh@gmail.com>
2023-05-03 13:35:22 +0800

    Add favicon

---
+++ apps/web/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
